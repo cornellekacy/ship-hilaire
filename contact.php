297ddddde6721f1083a6eedd7d6d3b18cb7fe009
cornellekacy <cornellekacy4@gmail.com>
2020-01-31 20:26:16 +0400

    2

--- contact.php
+++ contact.php
@@ -42,25 +42,25 @@ if (array_key_exists('email', $_POST)) {
     $mail->isSMTP();
      // $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
 $mail->SMTPSecure = 'tls';
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = 'smtp.yandex.com';
 $mail->Port = 587;
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "cornellekacy4@gmail.com";
+$mail->Username = "contact@cargomover-homeservice-delivery.com";
 //Password to use for SMTP authentication
-$mail->Password = "cornellekacy456";
+$mail->Password = "cargomoverhomeservice45";
     //Use a fixed address in your own domain as the from address
     //**DO NOT** use the submitter's address here as it will be forgery
     //and will cause your messages to fail SPF checks
-    $mail->setFrom('cornellekacy4@gmail.com', $_POST['name']);
+    $mail->setFrom('contact@cargomover-homeservice-delivery.com', $_POST['fname']);
     //Send the message to yourself, or whoever should receive contact for submissions
     $mail->addAddress('cornellekacy4@gmail.com', 'Contact');
     //Put the submitter's address in a reply-to header
     //This will fail if the address provided is invalid,
     //in which case we should ignore the whole request
-    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
-        $mail->Subject = 'Petfly Relocation';
+    if ($mail->addReplyTo($_POST['email'], $_POST['fname'])) {
+        $mail->Subject = 'cargomover homeservice delivery';
         //Keep it simple - don't use HTML
         $mail->isHTML(false);
         //Build a simple message body
